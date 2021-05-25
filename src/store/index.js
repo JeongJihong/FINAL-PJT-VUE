e76fdf1329a7e17695460b1ed06c384618bea051
lsh104305 <lsh104305@naver.com>
2021-05-25 11:09:06 +0900

    로그인 수정

--- src/store/index.js
+++ src/store/index.js
@@ -141,9 +141,10 @@ export default new Vuex.Store({
       state.member = member;
     },
 
-    LOGIN_MEMBER(state, id) {
-      state.loginId = id;
+    LOGIN_MEMBER(state, member) {
+      state.loginId = member.id;
       state.loginState = true;
+      state.member = member;
     },
 
     LOGOUT_MEMBER(state) {
@@ -445,8 +446,8 @@ export default new Vuex.Store({
         });
     },
 
-    loginMember({ commit }, id) {
-      commit("LOGIN_MEMBER", id);
+    loginMember({ commit }, member) {
+      commit("LOGIN_MEMBER", member);
     },
 
     insertMember({ commit }, member) {
