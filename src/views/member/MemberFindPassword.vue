18711a58a9c304c1eb776bde5dbbfafcd00aad3f
lsh104305 <lsh104305@naver.com>
2021-05-26 15:04:35 +0900

    비밀번호 찾기 수정

--- src/views/member/MemberFindPassword.vue
+++ src/views/member/MemberFindPassword.vue
@@ -1,60 +1,55 @@
 <template>
-  <div>
-    <!-- main contents -->
-    <section id="main-contents" class="container-fluid">
-      <!-- content -->
-      <div class="row mb-2">
-        <!-- AD -->
-        <article class="col-sm-4">
-          <img src="@/assets/img/sideAD.jpg" class="img-thumbnail" alt="ad" />
-        </article>
-
-        <!-- 비밀번호 찾기 -->
-        <article class="col-sm-8">
-          <!-- 비밀번호 찾기 창-->
-          <div id="head" class="border rounded">
-            <form method="post" action="/mem/findpassword" class="container-container-fluid">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <td class="caption" colspan="2">비밀번호 찾기</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class="tbasic">아 이 디</td>
-                    <td class="tbasic"><input type="text" name="id" id="id" value="" /></td>
-                  </tr>
-                  <tr>
-                    <td class="tbasic">이 &nbsp;름</td>
-                    <td class="tbasic"><input type="text" name="name" id="name" value="" /></td>
-                  </tr>
-                  <tr>
-                    <td class="tbasic">이메일</td>
-                    <td class="tbasic"><input type="text" name="email" id="email" value="" /></td>
-                  </tr>
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <td colspan="2" align="center" class="tfoot tspacial">
-                      <!-- <input class="btn btn-outline-primary" type="submit" value="검색" /> -->
-                      <router-link to="/" class="btn btn-outline-primary">검색</router-link>
-                      <router-link to="/" class="btn btn-outline-secondary">취소</router-link>
-                    </td>
-                  </tr>
-                </tfoot>
-              </table>
-              <span v-if="msg != null" class="text-danger">${msg}</span>
-            </form>
-          </div>
-        </article>
+  <div class="wrapper fadeInDown">
+    <div id="formContent">
+      <br>
+      <h1>비밀번호 찾기</h1>
+      <input type="text" class="inputLogin" v-model="id" placeholder="id"/>
+      <input type="text" class="inputLogin" v-model="name" placeholder="name"/>
+      <input type="text" class="inputLogin" v-model="email" placeholder="email"/>
+      <input type="submit" @click.prevent="findPassword" class="loginBtn" value="검색"/>
+      <div class="text-danger">{{ msg }}</div>
+      <br>
+      <div id="formFooter">
+        <router-link to="/" class="underlineHover">취소</router-link>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      id: "",
+      name: "",
+      email: "",
+      msg: "",
+    };
+  },
+
+  methods: {
+    findPassword() {
+      const addr = "http://localhost/mem/findpassword";
+
+      axios
+        .post(addr, { id: this.id, name: this.name, email: this.email })
+        .then((response) => {
+          // console.log(response);
+          this.msg = response.data.msg;
+        })
+        .catch((error) => {
+          this.msg = response.data.msg;
+          console.dir(error);
+        });
+    },
+  },
+
+  created() {
+    this.msg = "";
+  },
+};
 </script>
 
 <style>
